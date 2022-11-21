230dbb413070d0308b4658238ef0655b8f15ad33
lamnaourm <mohammed.lamnaour@gmail.com>
2022-11-21 17:11:31 +0100

    third commit

--- src/redux/BudgetSlice.jsx
+++ src/redux/BudgetSlice.jsx
@@ -4,8 +4,25 @@ import { createSlice } from "@reduxjs/toolkit";
 const BudgetSlice = createSlice({
     name:'budget',
     initialState:{
-        budget:0,
-        depenses: []
+        budget:2500,
+        lastid:3,
+        depenses: [
+            {
+                id:1,
+                description: 'Diesel Voiture',
+                montant:800
+            },
+            {
+                id:2,
+                description: 'Achat PC',
+                montant:8000
+            },
+            {
+                id:3,
+                description: 'Achat Cartable',
+                montant:450
+            },
+        ]
     },
     reducers: {}
 })
